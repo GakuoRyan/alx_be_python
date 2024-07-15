6c96d41dd165a40208015640187621674d0fff9d
GakuoRyan <rwgakuo@gmail.com>
2024-07-15 11:51:18 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,6 +12,6 @@ class BankAccount:
         self.account_balance -= amount
         return True
     else:
-      print("Insufficient funds.")
+        return False
   def display_balance(self):
-    print(f"Current Balance: ${self.account_balance:.2f}")
+    print(f"Current Balance: ${self.account_balance:.2f}"))
